56f49d37bc0d5602e0f79d95bdc20bebf6157deb
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-04-26 22:36:20 -0400

    lesson 1: types and data structures

---
+++ Intro/Main.java
@@ -0,0 +1,127 @@
+
+// import java.util.Vector;
+// import java.util.ArrayList;
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+    // int num = 3;
+    // if(num == 1){
+    // System.out.println("1");
+    // } else if(num == 2){
+    // System.out.println("2");
+    // } else {
+    // System.out.println("3");
+    // }
+
+    // for(int i=0;i<5;i++){
+    // System.out.println("Hello World");
+    // }
+
+    // int count = 10;
+    // while(count > 0){
+    // System.out.println("Hello World");
+
+    // count--;
+    // }
+
+    // do {
+    // System.out.println("Hello World");
+    // count--;
+    // } while(count > 0);
+    // String word1 = new String();
+    // String word2 = new String();
+    // word1 = "test";
+    // word2 = "test";
+    // if(word1 == word2){
+    // System.out.println("Words are matching");
+    // }
+
+    // String word = "Hello Universe";
+    // switch(word){
+    // case "Hello Universe":
+    // System.out.println("universe");
+    // break;
+    // case "Hello World":
+    // System.out.println("world");
+    // break;
+    // default:
+    // System.out.println("default");
+    // }
+
+    // Long longNum = 5L;
+    // System.out.println(longNum);
+
+    // Float decimal = 3.14F;
+    // System.out.println(decimal);
+
+    // OOP
+
+    // Object // toString, equals
+
+    // Number, string, Big int, symbol, boolean, undefined, null
+    // let num = 5; //JS
+
+    // byte bytey = 100; // Primative
+    // Byte bytey = 1; // Wrapper
+
+    // short num = 5; // 2bytes
+    // Short num = 5;
+
+    // int num = 5; // 4bytes
+    // Integer num = 5;
+
+    // long num = 5L; // 8bytes
+    // Long num = 5L;
+
+    // // System.out.println(num.toString());
+
+    // float floaty= 3.14F;
+    // Float floaty = 3.14F;
+
+    // double doubley = 3.14;
+    // Double doubley = 3.14;
+
+    // boolean bool = true; // 1bit
+    // Boolean bool = false;
+
+    // char chary = 'c';
+    // Character chary = 'c';
+
+    // String stringy = "Hello World";
+
+    // Object, Array, Set, Map
+    // Integer[] numArr = {1,2,3}; //Array - Set length
+
+    // for(int i=0; i<numArr.length; i++){
+    // System.out.println(numArr[i]);
+    // }
+
+    // Vector<Integer> vecty = new Vector<>(2, 5); // Vector<>(initialCapacity,
+    // capacityIncrementor);
+    // vecty.add(10); // [10, null]
+    // vecty.add(42); // [10, 42]
+    // vecty.add(15); // [10, 42, 15, null, null]
+    // System.out.println(vecty.capacity());
+
+    // ArrayList<Integer> listy = new ArrayList<>(); // [10,20,30] [0,1,2]\
+    // listy.add(1);
+    // listy.add(2);
+    // listy.add(3);
+    // System.out.println(listy);
+
+    // LinkedList<Integer> linky = new LinkedList<>();
+    // linky.add(10);
+    // linky.add(20);
+    // linky.add(30);
+    // System.out.println(linky.peek());
+
+    // HashMap<String, Integer> hashy = new HashMap<>();
+    // hashy.put("age", 13);
+
+    // Integer age = hashy.get("age");
+    // System.out.println(age);
+
+  }
+}
